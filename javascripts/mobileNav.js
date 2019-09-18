1078eb898e28c74a02914dfe933a67654a8f960d
Stanley Huang <huang.stanley050@gmail.com>
2019-09-18 11:35:45 +1000

    mobile navbar done

---
+++ javascripts/mobileNav.js
@@ -0,0 +1,12 @@
+const mobile_nav_menu = document.querySelector(".mobile-nav__container");
+const hamburger_icon = document.getElementById("hamburger");
+const show_nav = () => {
+  console.log("showing nav");
+  if (mobile_nav_menu.style.display === "none") {
+    mobile_nav_menu.style.display = "block";
+  } else {
+    mobile_nav_menu.style.display = "none";
+  }
+};
+
+hamburger_icon.addEventListener("click", show_nav);
