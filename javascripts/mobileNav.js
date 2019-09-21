37ae21301e6d4f33b755786e0ca53d4cc5f1f53c
Stanley Huang <huang.stanley050@gmail.com>
2019-09-21 20:48:46 +1000

    fixed major issue with mobile nav, whick only works on 2nd click

--- javascripts/mobileNav.js
+++ javascripts/mobileNav.js
@@ -1,7 +1,12 @@
 const mobile_nav_menu = document.querySelector(".mobile-nav__container");
 const hamburger_icon = document.getElementById("hamburger");
 const show_nav = () => {
-  if (mobile_nav_menu.style.display === "none") {
+  if (
+    mobile_nav_menu.style.display === "none" ||
+    mobile_nav_menu.style.display === "" //this line fixed the issue relates to:
+    //https://stackoverflow.com/questions/31147104/onclick-trigger-doesnt-work-first-click
+    //because when the display is set to none, it doesn't respond with correct value;
+  ) {
     mobile_nav_menu.style.display = "block";
   } else {
     mobile_nav_menu.style.display = "none";
